b60dbe905a03cd0a2f6dbdbb4a8057fb5d3159f6
Bardini <derekbardini@gmail.com>
2021-03-09 23:47:02 -0600

    deployed but with some problems

---
+++ client/public/navscript.js
@@ -0,0 +1,10 @@
+// $(function() {
+//     $(window).scroll(function() {
+//       var winTop = $(window).scrollTop();
+//       if (winTop >= 10) {
+//         $("body").addClass("sticky-shrinknav-wrapper");
+//       } else if (winTop >= 1) {
+//         $("body").removeClass("sticky-shrinknav-wrapper");
+//       }
+//     });
+//   });
